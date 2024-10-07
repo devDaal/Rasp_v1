b9daabd3aee64fe78dedceaea62aede78e6d6d92
devDaal <diegoaloz98993@gmail.com>
2024-10-07 12:56:05 -0600

    First set of settings of the sensor page

--- Tests/GUIv1.py
+++ Tests/GUIv1.py
@@ -230,7 +230,7 @@ class Motor(Frame):
        self.motor_selected = StringVar()
        self.motor_selected.set('1')
        
-       #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+       #-------------------------------------------------------------------------Frame & Title---------------------------------------------------------------------------------------
        
        self.motor_container= Frame(self,bg='gray',padx=400,pady=10)
        self.motor_container.pack(expand="True", fill='both')
@@ -376,14 +376,53 @@ class Sensor(Frame):
     def __init__(self):
        Frame.__init__(self) 
        
-       self.encoder_container= Frame(self,bg='gray',padx=400,pady=10)
-       self.encoder_container.pack(expand="True", fill='both')
+       #--------------------------------------------------------------------------Frame & Title---------------------------------------------------------------------------------------
        
-       self.encoder_label=Label(self.encoder_container, text="Sensors",borderwidth=2,bg="gray",fg="white",bd=2,font=("Robot", 30,"bold"))
-       self.encoder_label.grid(pady=5)
+       self.sensor_container= Frame(self,bg='gray',padx=400,pady=10)
+       self.sensor_container.pack(expand="True", fill='both')
        
-       self.encoder_exit_btn = Button(self.encoder_container, text='EXIT',bg='red',fg='white',font=("Robot", 25,"bold"), command=lambda: app.show_frame(StartPage))
+       self.sensor_label=Label(self.sensor_container, text="Sensors",borderwidth=2,bg="gray",fg="white",bd=2,font=("Robot", 30,"bold"))
+       self.sensor_label.grid(pady=5)
+       
+       #---------------------------------------------------------------------------Variables------------------------------------------------------------------------------------------
+       
+       self.sensor_selected = IntVar()
+       self.sensor_selected.set(1)
+       
+       #----------------------------------------------------------------------------Settings -----------------------------------------------------------------------------------------
+       
+       self.sensor_setings_container = LabelFrame(self.sensor_container, bg="#4f4f4f",width='200', height='200',fg="white", pady = 20)
+       self.sensor_setings_container.grid(row=1, pady=10, ipady=10)
+       
+       self.quantity_sensors_lbl = Label(self.sensor_setings_container, text='How many sensors?', bg="#4f4f4f",font=("Robot", 16,"bold"))
+       self.quantity_sensors_lbl.grid()
+       
+       #-----------------------------------------------------------
+       
+       values = {"1" : "1",
+                "2" : "2",
+                "3" : "3",
+                "4" : "4",
+                "5" : "5",
+                "6" : "6",
+                "7" : "7",
+                "8" : "8",
+                "9" : "9"}
+       
+       for (text, value) in values.items():
+            temp_radio = Radiobutton(self.sensor_setings_container,bg="#4f4f4f", text = text, font=("Robot", 18,"normal"), variable = self.sensor_selected,value = value)#,command=self.click_btn_radios)
+            temp_radio.grid()
+       
+       #-----------------------------------------------------------
+       
+       self.selected_btn = Button(self.sensor_setings_container, text='Select Sensor(s)', bg='green',fg='White',font=("Robot", 16,"bold"), command=self.get_selected_sensor)
+       self.selected_btn.grid()
+       
+       self.encoder_exit_btn = Button(self.sensor_container, text='EXIT',bg='red',fg='white',font=("Robot", 25,"bold"), command=lambda: app.show_frame(StartPage))
        self.encoder_exit_btn.grid()
+       
+    def get_selected_sensor(self):
+        self.test_amount_sensor = self.sensor_selected.get()
 
 if __name__ == "__main__":
     app = App()
